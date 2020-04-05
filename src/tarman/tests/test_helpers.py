bc997b3653ac15bc94ef1cc95746eb36108c028f
Matej Cotman <cotman.matej@gmail.com>
2020-04-05 16:39:29 +0300

    update(test): remove unused LibArchive tests

--- src/tarman/tests/test_helpers.py
+++ src/tarman/tests/test_helpers.py
@@ -1,9 +1,3 @@
-from tarman.containers import LibArchive
-from tarman.helpers import container
-from tarman.helpers import get_archive_class
-from tarman.helpers import utf8_return
-from tarman.helpers import utf8_args
-
 import os
 import tarman.tests.test_containers
 import unittest2 as unittest
@@ -18,31 +12,20 @@ class TestHelpers(unittest.TestCase):
             self.testdirectory, 'testdata', 'testdata.tar.gz'
         )
 
-    def test_get_archive_class(self):
-        archive_class = get_archive_class(self.testarchivepath)
-        self.assertEqual(archive_class, LibArchive)
-
-    def test_container(self):
-        c = container(self.testarchivepath)
-        self.assertIsInstance(c, LibArchive)
-
-    @utf8_return
     def utf8_return_function(self):
         return "sheeps"
 
     def test_utf8_return(self):
-        self.assertIsInstance(self.utf8_return_function(), unicode)
+        self.assertIsInstance(self.utf8_return_function(), str)
 
-    @utf8_args(2)
     def utf8_args_by_index_function(self, text1=None, text2=None):
         return text1, text2
 
     def test_utf8_args_by_index(self):
         text1, text2 = self.utf8_args_by_index_function("sheeps1", "sheeps2")
         self.assertIsInstance(text1, str)
-        self.assertIsInstance(text2, unicode)
+        self.assertIsInstance(text2, str)
 
-    @utf8_args('text2')
     def utf8_args_by_keyword_function(self, text1=None, text2=None):
         return text1, text2
 
@@ -51,9 +34,8 @@ class TestHelpers(unittest.TestCase):
             "sheeps1", text2="sheeps2"
         )
         self.assertIsInstance(text1, str)
-        self.assertIsInstance(text2, unicode)
+        self.assertIsInstance(text2, str)
 
-    @utf8_args(1, 'text2')
     def utf8_args_combo_function(self, text1=None, text2=None):
         return text1, text2
 
@@ -62,4 +44,4 @@ class TestHelpers(unittest.TestCase):
             "sheeps1", text2="sheeps2"
         )
         self.assertIsInstance(text1, str)
-        self.assertIsInstance(text2, unicode)
+        self.assertIsInstance(text2, str)
